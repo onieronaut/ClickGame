7aecaefed947c3e4331fd2d92e420bc8f0521a65
jerard <qbin221@gmail.com>
2020-02-11 21:00:00 -0500

    Updates

---
+++ precache-manifest.868a966d0432e1ffc8586ea9410287bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdb51708c29f2d49a7217dd2e899325",
+    "url": "/ClickGame/index.html"
+  },
+  {
+    "revision": "9c6d8699c8369197aef1",
+    "url": "/ClickGame/static/css/main.194dc708.chunk.css"
+  },
+  {
+    "revision": "b5f393719daa4ea965e7",
+    "url": "/ClickGame/static/js/2.2b30e680.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ClickGame/static/js/2.2b30e680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6d8699c8369197aef1",
+    "url": "/ClickGame/static/js/main.020fbadf.chunk.js"
+  },
+  {
+    "revision": "55a971a9ae0b53969ab9",
+    "url": "/ClickGame/static/js/runtime-main.9f82b2b1.js"
+  }
+]);
