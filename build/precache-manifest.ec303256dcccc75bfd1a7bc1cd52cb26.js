f2188986867366cb3ce28f8977f92c10a6bc9573
jerard <qbin221@gmail.com>
2020-02-14 20:51:28 -0500

    configuring images for gh pages

---
+++ build/precache-manifest.ec303256dcccc75bfd1a7bc1cd52cb26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c93a11fef7a8124765aabc9983415e0",
+    "url": "/ClickGame/index.html"
+  },
+  {
+    "revision": "6d706e53f85fabdab99f",
+    "url": "/ClickGame/static/css/main.11a6652b.chunk.css"
+  },
+  {
+    "revision": "b5f393719daa4ea965e7",
+    "url": "/ClickGame/static/js/2.2b30e680.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ClickGame/static/js/2.2b30e680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d706e53f85fabdab99f",
+    "url": "/ClickGame/static/js/main.36e79632.chunk.js"
+  },
+  {
+    "revision": "55a971a9ae0b53969ab9",
+    "url": "/ClickGame/static/js/runtime-main.9f82b2b1.js"
+  }
+]);
